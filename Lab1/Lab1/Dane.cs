b3ee80bd3a0f4281d42410094262a1b96d4da9de
Kacper Michalak <michalakkacper2002@gmail.com>
2026-05-09 12:34:16 +0200

    feat(Lab1): add basic data classes

---
+++ Lab1/Lab1/Dane.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1
+{ 
+    public class Przychodnia
+    {
+        public string id { get; set; }
+        public string nazwa { get; set; }
+    }
+    
+    public class Pacjent
+    {
+        public string id { get; set; }
+        public string imie { get; set; }
+        public string nazwisko { get; set; }
+
+        public List<Przychodnia> przychodnie { get; set; }
+
+        public override string ToString()
+        {
+            return $"Pacjent{{id={id}, imie={imie}, nazwisko={nazwisko}}}"; ;
+        }
+
+    }
+}
