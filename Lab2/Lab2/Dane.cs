03189d728d022704ed32c2aa37413d83cad2e2f5
Kacper Michalak <michalakkacper2002@gmail.com>
2026-05-24 09:56:29 +0200

    feat(Lab2): add data models

---
+++ Lab2/Lab2/Dane.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2
+{
+    public class Samochod
+    {
+        public string? Id { get; set; }
+        public string Marka { get; set; } = string.Empty;
+        public string Model { get; set; } = string.Empty;
+        public int RokProdukcji { get; set; }
+        public string SerwisId { get; set; } = string.Empty;
+
+    }
+
+
+    public class Serwis
+    {
+        public string? Id { get; set; }
+        public string Nazwa { get; set; } = string.Empty;
+        public string Miasto { get; set; } = string.Empty;
+        public List<string> SamochodyId { get; set; } = new();
+    }
+}
